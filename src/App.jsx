a454492df00dc3a7aa49efcefe940f96c437ae89
Stanley Delva <juniordelva1@gmail.com>
2025-07-15 19:03:48 -0400

    fetch data setup function created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+const URL = `https://api.spoonacular.com/recipes/complexSearch?&number=20&apiKey=${ACCESS_KEY}`;
+const URL2 = `https://api.spoonacular.com/recipes/${716429}/information?apiKey=${ACCESS_KEY}`;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [recipe, setRecipes] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function getRecipe() {
+        async function fetchData() {
+            try {
+                const response = await fetch(URL);
+                const data = await response.json();
+                setRecipes(data.results);
+                console.log(recipe);
+            } catch (error) {
+                console.log("ERROR", error);
+            }
+        }
+        fetchData();
+    }
+    useEffect(() => {
+        // async function fetchData() {
+        //     try {
+        //         const response = await fetch(URL);
+        //         const data = await response.json();
+        //         setRecipes(data.results);
+        //         console.log(recipe);
+        //     } catch (error) {
+        //         console.log("ERROR: ", error);
+        //     }
+        // }
+        // fetchData();
+    }, []);
+
+    return (
+        <>
+            <button onClick={getRecipe}>Populate Recipe</button>
+        </>
+    );
 }
 
-export default App
+export default App;
